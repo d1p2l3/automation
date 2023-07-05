2d5fd45d2a436576abec08b49caecfec0cac7c6d
d1p2l3 <51164761+d1p2l3@users.noreply.github.com>
2023-07-05 18:24:37 +0530

    Update LoginTest.java

--- LoginTest.java
+++ LoginTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginTestScript {
+public class LoginTest {
     public static void main(String[] args) {
         // Assuming ChromeDriver is used and its executable is added to the system's PATH
         
@@ -21,11 +21,16 @@ public class LoginTestScript {
         login(driver, validUsername, validPassword);
         // Add assertion to verify successful login and access to the system
         
+        Assert.assertEquals("http://example.com/dashboard", driver.getCurrentUrl());
+        Assert.assertTrue(driver.findElement(By.id("welcome-message")).isDisplayed());
+
         // Test Case 2: Verify error message on invalid credentials
         String invalidUsername = "invalid_username";
         String invalidPassword = "invalid_password";
         login(driver, invalidUsername, invalidPassword);
         // Add assertion to verify error message
+        Assert.assertTrue(driver.findElement(By.id("error-message")).isDisplayed());
+
         
         // Test Case 3: Verify retained username and password fields on failed login
         // Assuming the username and password input fields have IDs: "username", "password"
@@ -35,47 +40,44 @@ public class LoginTestScript {
         driver.findElement(By.id("password")).sendKeys(enteredPassword);
         driver.findElement(By.id("login-button")).click(); // Assuming login button ID is "login-button"
         // Add assertions to verify the retained values in the input fields
+        Assert.assertEquals(enteredUsername, driver.findElement(By.id("username")).getAttribute("value"));
+        Assert.assertEquals(enteredPassword, driver.findElement(By.id("password")).getAttribute("value"));
+
         
-        // Test Case 4: Verify login button disabled on empty fields
-        // Assuming the login button has an attribute "disabled" when it is disabled
-        // Add assertion to verify the login button's disabled state
+        // Test Case 4: Test that the 'Login' button is disabled when both username and password fields are empty
+        Assert.assertFalse(driver.findElement(By.id("login-button")).isEnabled());
+
         
-        // Test Case 5: Verify login button enabled on valid input
-        // Assuming the login button has an attribute "disabled" when it is disabled
-        // Add assertions to verify the login button's enabled state
+        // Test Case 5: Confirm that the 'Login' button is enabled only when both username and password fields are filled with valid input
+        driver.findElement(By.id("username")).sendKeys(validUsername);
+        driver.findElement(By.id("password")).sendKeys(validPassword);
+        Assert.assertTrue(driver.findElement(By.id("login-button")).isEnabled());
+
         
         // Step 3: Test password recovery functionality
         
-        // Test Case 6: Verify password recovery redirection
+        // Test Case 6: Verify that clicking the 'Forgot Password' link redirects the user to the password recovery page
         driver.findElement(By.linkText("Forgot Password")).click();
-        // Add assertion to verify the password recovery page redirection
+        Assert.assertEquals("http://example.com/password-recovery", driver.getCurrentUrl());
+    
         
         // Test Case 7: Validate password recovery process
         String email = "test@example.com"; // Replace with a valid email address
         driver.findElement(By.id("email")).sendKeys(email);
         driver.findElement(By.id("recover-password-button")).click();
         // Add assertions to verify the success message
-        
-        // Simulate the password recovery process using external tools or a test email account
-        
-        // Add assertions to verify successful password reset, redirection to the login page, and login with the new password
+        By.id("recover-password-button")).click();
+        // Verify that a success message is displayed indicating that an email with password recovery instructions has been sent
+        Assert.assertTrue(driver.findElement(By.id("success-message")).isDisplayed());
+
         
         // Step 4: Test CAPTCHA integration
         
-        // Test Case 8: Verify CAPTCHA challenge
-        // Add assertions to verify the presence of the CAPTCHA challenge
-        
-        // Test Case 8: Verify CAPTCHA error message
-        driver.findElement(By.id("login-button")).click();
-        // Add assertion to verify the CAPTCHA error message
-        
-        // Test Case 9: Verify successful login with solved CAPTCHA
-        // Simulate solving the CAPTCHA challenge
-        driver.findElement(By.id("login-button")).click();
-        // Add assertions to verify successful login
         
         // Close the browser
         driver.quit();
+    }
+    
     }
     
     
